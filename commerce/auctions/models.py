d014a2551fd826e77411c349ff489c4ffd4bc4d7
Naomi Stuart <nstu778@aucklanduni.ac.nz>
2020-09-03 23:40:27 +1000

    start active listings and create listings pages for project 2 - auctions

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,38 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    LISTING_CATEGORIES = [
+        ('BOOKS', 'Books'),
+        ('MUSIC', 'Music'),
+        ('MOVIES', 'Movies'),
+        ('GAMES', 'Games'),
+        ('COMPUTERS', 'Computers'),
+        ('ELECTRONICS', 'Electronics'),
+        ('KITCHEN', 'Kitchen'),
+        ('HOME', 'Home'),
+        ('HEALTH', 'Health'),
+        ('PETS', 'Pets'),
+        ('TOYS', 'Toys'),
+        ('FASHION', 'Fashion'),
+        ('SHOES', 'Shoes'),
+        ('SPORTS', 'Sports'),
+        ('BABY', 'Baby'),
+        ('TRAVEL', 'Travel')
+    ]
+
+    title = models.CharField(max_length=200, verbose_name="Title")
+    description = models.TextField(verbose_name="Description")
+    starting_bid = models.DecimalField(decimal_places=2, verbose_name="Starting Bid", max_digits=15)
+    image = models.URLField(blank=True, verbose_name="Image URL", null=True)
+    category = models.CharField(choices=LISTING_CATEGORIES, blank=True, verbose_name="Category", max_length=200, null=True)
+
+    def __str__(self):
+        return f"{self.id}: {self.title}"
+
+class Bid(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -1,14 +1,38 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.forms import ModelForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
-from .models import User
+from .models import User, Listing, Bid, Comment
+
+class NewListingForm(ModelForm):
+    class Meta:
+        model = Listing
+        fields = ['title', 'description', 'starting_bid', 'image', 'category']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-newListingForm'
+        self.helper.form_class = 'blueForms'
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'create'
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-8'
+
+        self.helper.add_input(Submit('submit', 'Submit'))
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    return render(request, "auctions/index.html", {
+        "listings": Listing.objects.all()
+    })
 
 
 def login_view(request):
@@ -61,3 +85,29 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+@login_required
+def create(request):
+    if request.method == "POST":
+        form = NewListingForm(request.POST)
+
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            description = form.cleaned_data["description"]
+            starting_bid = form.cleaned_data["starting_bid"]
+            image = form.cleaned_data["image"]
+            category = form.cleaned_data["category"]
+
+            listing = Listing(title=title, description=description, starting_bid=starting_bid, image=image, category=category)
+            listing.save()
+            return HttpResponseRedirect(reverse("index"))
+        
+        else:
+            return render(request, "auctions/create.html", {
+                "listing_form": form
+            })
+
+    return render(request, "auctions/create.html", {
+        "listing_form": NewListingForm()
+    })
