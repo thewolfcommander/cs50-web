30f1ae2980f5c2e00299eed18d81e50935568e36
Naomi Stuart <nstu778@aucklanduni.ac.nz>
2020-09-18 22:34:53 +1000

    register models to admin page

--- mail/mail/admin.py
+++ mail/mail/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . models import User, Email
+
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "sender", "subject", "timestamp", "read", "archived")
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Email, EmailAdmin)
