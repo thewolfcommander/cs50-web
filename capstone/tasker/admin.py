e8402d4ba657354585b7b0a184dfd76390631454
Naomi Stuart <nstu778@aucklanduni.ac.nz>
2020-09-20 20:34:47 +1000

    set up models for capstone project

--- capstone/tasker/admin.py
+++ capstone/tasker/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . models import User, Task, Offer, Question
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "poster", "assignee", "budget", "timestamp", "status")
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ("id", "task", "tasker", "timestamp")
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("id", "commenter", "timestamp", "content")
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Offer, OfferAdmin)
+admin.site.register(Question, QuestionAdmin)
--- capstone/tasker/models.py
+++ capstone/tasker/models.py
@@ -1,3 +1,61 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+
+class Task(models.Model):
+    TASK_CATEGORIES = [
+        ('Furniture Assembly', 'Furniture Assembly'),
+        ('Delivery', 'Delivery'),
+        ('Home & Gardening', 'Home & Gardening'),
+        ('Business & Admin', 'Business & Admin'),
+        ('Computers & IT', 'Computers & IT'),
+        ('Marketing & Design', 'Marketing & Design'),
+        ('Events & Photography', 'Events & Photography'),
+        ('Tutoring & Training', 'Tutoring & Training'),
+        ('Other', 'Other')
+    ]
+
+    TASK_STATUSES = [
+        ('Open', 'Open'),
+        ('Assigned', 'Assigned'),
+        ('Completed', 'Completed'),
+        ('Cancelled', 'Cancelled')
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posted_tasks")
+    due_date = models.DateField()
+    budget = models.IntegerField()
+    category = models.CharField(choices=TASK_CATEGORIES, max_length=200)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=TASK_STATUSES, max_length=200, default="Open")
+    assignee = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL, default=None, related_name="assigned_tasks")
+
+    def __str__(self):
+        return f"(ID: {self.id}) {self.title}"
+
+
+class Offer(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    tasker = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.tasker} offered to do {self.task} for ${self.price}"
+
+
+class Question(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.commenter} commented on {self.task}"
+
