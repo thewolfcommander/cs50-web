2f5818cf7e2e66f74c9cff6cb5c47a6672678ec6
Manoj Pawan Tyagi <38884133+thewolfcommander@users.noreply.github.com>
2020-10-01 20:01:49 +0530

    Added Admin Decorators instead registering models separately
    I have added Admin Decorators instead of registering models and admin classes separately to increase code reusability.

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -2,24 +2,21 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from . models import User, Listing, Bid, Comment
 
+@admin.register(Listing)
 class ListingAdmin(admin.ModelAdmin):
     list_display = ("id", "creator", "title", "timestamp", "price", "category", "active", "winner")
 
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ("id", "commenter", "timestamp", "content")
 
+@admin.register(Bid)
 class BidAdmin(admin.ModelAdmin):
     list_display = ("id", "listing", "bidder", "timestamp", "bid_price")
 
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
     filter_horizontal = ("watchlist",)
     fieldsets = UserAdmin.fieldsets + (
         ("Watchlist", {'fields': ('watchlist',)}),
     )
-
-# Register your models here.
-admin.site.register(Listing, ListingAdmin)
-admin.site.register(Comment, CommentAdmin)
-admin.site.register(Bid, BidAdmin)
-admin.site.register(User, CustomUserAdmin)
-
